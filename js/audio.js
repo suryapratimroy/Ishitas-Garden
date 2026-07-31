50da5cd958c143386fd6b4e5adfcab35dd62da47
Surya Pratim Roy <suryapratimroy052@gmail.com>
2026-07-31 12:02:18 +0530

    Initial website

---
+++ js/audio.js
@@ -0,0 +1,10 @@
+// const myAudio = new Audio("./love-story.mp3");
+
+// myAudio.loop = true;
+
+// document.addEventListener("click", () => {
+//     console.log("Clicked");
+//     myAudio.play()
+//         .then(() => console.log("Playing"))
+//         .catch(err => console.log(err));
+// }, { once: true });
